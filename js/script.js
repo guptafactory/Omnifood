fc38541eabb05a614d407482bb3e0afa6065c7ed
Himanshu Gupta <guptafactory@gmail.com>
2024-10-05 20:36:23 +0530

    Add: Sticky Navbar & Favicons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Mobile Navigation
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const sectionHeroEl = document.querySelector(".section-hero");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
@@ -32,6 +33,24 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky Navigation
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    // respect to viewport and when 0% visible
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -45,7 +64,6 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
